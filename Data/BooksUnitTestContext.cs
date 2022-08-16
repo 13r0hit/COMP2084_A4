7fecebf47800296bb4a991ab25bb193ab94da3e7
Rohit Rohit <200477100@student.georgianc.on.ca>
2022-08-16 20:19:42 +0100

    second commit

---
+++ Data/BooksUnitTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BooksUnitTest.Models;
+
+namespace BooksUnitTest.Data
+{
+    public class BooksUnitTestContext : DbContext
+    {
+        public BooksUnitTestContext (DbContextOptions<BooksUnitTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BooksUnitTest.Models.Student> Student { get; set; } = default!;
+    }
+}
